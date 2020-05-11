74235f8068e3b7142193a72d470722ae980438e4
Anna <annamorrison500@hotmail.com>
2020-05-11 21:06:17 +0100

    Modal JS
    - removed 2 console logs as they were no longer needed
    - added comments against most lines
    -completed modal duration and close JS
    -need to add start JS, modal is appearing on page load not after button animation has ended

--- memorygame.js
+++ memorygame.js
@@ -8,21 +8,42 @@ begin.addEventListener('click', showCorrect);
 
 //get the correct tiles
 var correctTiles = document.getElementsByClassName('grid-item-lg correct');
-console.log(correctTiles);
-console.log(correctTiles.classList);
 
 //function to display combination
 function showCorrect(el) {
     Array.from(correctTiles).forEach(function(el) {
-        el.classList.add("active");
+        el.classList.add("active"); //add "ative" animation class
         setTimeout(function(){
-            el.classList.remove('active');
-            begin.removeEventListener('click', showCorrect);
-        },2000);
+            el.classList.remove('active'); //remove "active" animation class
+            begin.removeEventListener('click', showCorrect); //remove begin button event listener
+        },2000); //2 seconds after the class is added
     })
-    
 }
 
+//countdown modal start
+//need to add class countdown-modal-open
+//need to have an event listener for the above function ending
+
+//countdown modal duration
+var countdown = document.getElementById("countdown-backdrop");
+var timeleft = 3;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){ //when the timer hits 0
+    clearInterval(downloadTimer);
+    document.getElementById("countdown-modal").innerHTML = "0"; //show 0s seconds remaining
+    closeModal(); //call modal close function
+  } else {
+    document.getElementById("countdown-modal").innerHTML = timeleft; //show time remaining
+    document.getElementById('countdown-backdrop').style.display = "block"; //display content
+  }
+  timeleft -= 1;
+}, 1000); // use seconds
+
+//countdown modal close function
+function closeModal(){
+    countdown.classList.remove('countdown-modal-open'); //remove open class from countdown-backdrop
+    countdown.classList.add('countdown-modal-close') //add close class to countdown-backdrop
+}
 
 
 
